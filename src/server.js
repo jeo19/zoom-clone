9fb6f57ae8ef2bccd597d7e2607e93d35550bebb
jeo19 <terrakkk@outlook.com>
2022-01-05 15:53:50 +0800

    feat:Add the pug templete file for home templete

--- src/server.js
+++ src/server.js
@@ -1,4 +1,7 @@
 import express from "express";
 const app = express();
-console.log("Hello");
-app.listen(3000);
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.get("/", (req, res) => res.render("home"));
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+app.listen(3000, handleListen);
