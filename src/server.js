a08ac77be95de6b6c76bc252ccee0d2679a1ae32
jeo19 <terrakkk@outlook.com>
2022-02-08 10:03:35 +0800

    feat:Look at the useful methods of a socket

--- src/server.js
+++ src/server.js
@@ -14,8 +14,13 @@ const httpServer = http.createServer(app);
 
 const wsServer = new Server(httpServer);
 wsServer.on("connection", (socket) => {
+  socket.onAny((event) => {
+    console.log(`Socket event:${event}`);
+  });
   socket.on("enter_room", (roomName, done) => {
-    console.log(roomName);
+    console.log(socket.rooms);
+    socket.join(roomName);
+    console.log(socket.rooms);
     setTimeout(() => {
       done("hello from the backend");
     }, 5000);
