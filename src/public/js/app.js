c206fdc4fa5c9297c7ababe662802d4d7cc42c3b
jeo19 <terrakkk@outlook.com>
2022-02-09 09:09:21 +0800

    feat:Show the room name in the room template

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -4,16 +4,20 @@ const welcome = document.getElementById("welcome");
 const form = welcome.querySelector("form");
 const room = document.getElementById("room");
 room.hidden = true;
+let roomName;
 
 function showRoom(msg) {
   welcome.hidden = true;
   room.hidden = false;
+  const h3 = room.querySelector("h3");
+  h3.innerText = `Room ${roomName}`;
 }
 
 function handleRoomSubmit(event) {
   event.preventDefault();
   const input = form.querySelector("input");
   socket.emit("enter_room", input.value, showRoom);
+  roomName = input.value;
   input.value = "";
 }
 form.addEventListener("submit", handleRoomSubmit);
