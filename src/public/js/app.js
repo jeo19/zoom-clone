20418d8787b1ba1f75b1558303a67bbca90105be
jeo19 <terrakkk@outlook.com>
2022-02-28 09:08:50 +0800

    feat:Get the camera list in browser

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -6,6 +6,16 @@ let myStream;
 let muted = false;
 let cameraOff = false;
 
+async function getCameras() {
+  try {
+    const devices = await navigator.mediaDevices.enumerateDevices();
+    const cameras = devices.filter((device) => device.kind === "videoinput");
+    console.log(cameras);
+  } catch (e) {
+    console.log(e);
+  }
+}
+
 async function getMedia() {
   try {
     myStream = await navigator.mediaDevices.getUserMedia({
@@ -13,6 +23,7 @@ async function getMedia() {
       video: true,
     });
     myFace.srcObject = myStream;
+    await getCameras();
   } catch (e) {
     console.log(e);
   }
