8e1c11246899490c747fb22b11f869c21f668bd9
jeo19 <terrakkk@outlook.com>
2022-03-01 09:31:05 +0800

    feat:Select the camera step02

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -12,10 +12,14 @@ async function getCameras() {
   try {
     const devices = await navigator.mediaDevices.enumerateDevices();
     const cameras = devices.filter((device) => device.kind === "videoinput");
+    const currentCamera = myStream.getVideoTracks()[0];
     cameras.forEach((camera) => {
       const option = document.createElement("option");
       option.value = camera.deviceId;
       option.innerText = camera.label;
+      if (currentCamera.label === camera.label) {
+        option.selected = true;
+      }
       cameraSelect.appendChild(option);
     });
   } catch (e) {
