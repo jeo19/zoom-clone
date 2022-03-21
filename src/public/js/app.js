44a76a6837c178e18705ff7f20172d40463ab01f
jeo19 <terrakkk@outlook.com>
2022-03-21 08:01:23 +0800

    feat:Add the stun server adress list in a peerConnection

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -147,7 +147,20 @@ socket.on("ice", (ice) => {
 
 //RTC Code
 function makeConnection() {
-  myPeerConnection = new RTCPeerConnection();
+  myPeerConnection = new RTCPeerConnection({
+    iceServers: [
+      {
+        urls: [
+          "stun:stun.l.google.com:19302",
+          "stun:stun1.l.google.com:19302",
+          "stun:stun2.l.google.com:19302",
+          "stun:stun3.l.google.com:19302",
+          "stun:stun4.l.google.com:19302",
+        ],
+      },
+    ],
+  });
+
   myPeerConnection.addEventListener("icecandidate", handleIce);
   myPeerConnection.addEventListener("addstream", handleAddStream);
   //Create the connection of an audio and a video by myStream on RTC peer to peer
